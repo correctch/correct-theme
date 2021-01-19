2a882e47c61f2682ee23999f286909568bd6289e
SamuelSchwegler <samuel.schwegler@gmx.ch>
2021-01-19 17:17:53 +0100

    post

--- single.php
+++ single.php
@@ -1,26 +1,32 @@
 <?php get_header(); ?>
-<div class="container row">
-    <div class="img-section col">
-        <?php if (has_post_thumbnail()){
-            if(wp_is_mobile()) {
+<div class="container">
+    <div class="img-section">
+        <?php if (has_post_thumbnail()) {
+            if (wp_is_mobile()) {
                 the_post_thumbnail('banner-image', ['class' => 'main-img']);
             } else {
                 the_post_thumbnail('side-image', ['class' => 'main-img']);
             }
         } ?>
     </div>
-    <div class="content col">
+    <div class="text-container" id="page-content">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <h2 id="post-title"><?php the_title(); ?></h2>
-            <div id="meta">
-                <p>erstellt am: <?php the_date('d.m.Y'); ?> |
-                    Kategorie: <?php the_category(', '); ?></p>
-            </div>
+            <h2 class="page-title"><?php the_title(); ?></h2>
             <div class="abstract">
                 <?php echo get_extended(get_post()->post_content)['main'] ?>
             </div>
             <div class="text">
                 <?php the_content(null, true); ?>
+            </div>
+            <div id="meta">
+                <div class="author-img">
+                    <?php echo get_avatar(get_the_author_meta('ID'), 100); ?>
+                </div>
+                <div class="author-meta">
+                    <br>
+                    <?php the_author_meta('first_name'); ?> <?php the_author_meta('last_name'); ?><br>
+                    <?php the_date('d.m.Y'); ?>
+                </div>
             </div>
         <?php endwhile; endif; ?>
     </div>
