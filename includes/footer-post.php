45f15eb93ecc3a359730fec19b295e366eea0052
SamuelSchwegler <samuel.schwegler@gmx.ch>
2021-01-21 17:00:46 +0100

    include footer post

--- includes/footer-post.php
+++ includes/footer-post.php
@@ -4,12 +4,13 @@ $the_query = new WP_Query( array(
     'post_type'      => 'post',
     'orderby'        => 'rand',
     'posts_per_page' => 1,
-) ); ?>
+    'post__not_in' => [get_the_ID()]
+) );
 
-<?php
+$on_post = is_single() && 'post' == get_post_type();
+
 // If we have posts lets show them
-if ( $the_query->have_posts() ) : ?>
-    <?php
+if ( $the_query->have_posts() ) :
     // Loop through the posts
     while ( $the_query->have_posts() ) : $the_query->the_post();  ?>
         <div id="footer-blog-post" class="text-container">
@@ -18,7 +19,13 @@ if ( $the_query->have_posts() ) : ?>
                     <?php the_post_thumbnail('card', ['class' => 'main-img']); ?>
                 </div>
                 <div class="col text-part">
-                    <span class="title">Blog</span>
+                    <span class="title">
+                        <?php if($on_post) { ?>
+                            Weiterer Beitrag lesen
+                        <?php } else { ?>
+                            Blog
+                        <?php } ?>
+                    </span>
                     <p><?php the_title(); ?></p>
                     <span class="date"><?php the_date('d.m.Y'); ?></span>
                     <a class="button" href="<?php the_permalink(); ?>">Beitrag lesen</a>
