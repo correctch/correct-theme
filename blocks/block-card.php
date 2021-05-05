04d6b486076e72353e80989ab0244da0a4ce3e1a
Samuel Schwegler <samuel.schwegler@gmx.ch>
2021-05-05 12:30:20 +0200

    versuch cards besser darzustellen

--- blocks/block-card.php
+++ blocks/block-card.php
@@ -2,5 +2,7 @@
     <img src="<?php block_field('bild'); ?>" alt="" class="card-hover-img">
     <span class="card-title"><?php block_field('titel'); ?></span>
     <p><?php block_field('text'); ?></p>
-    <a href="<?php block_field('url'); ?>" class="card"></a>
+    <?php if(block_field('url') !== "") { ?>
+        <a href="<?php block_field('url'); ?>" class="card-link button">mehr erfahren</a>
+    <?php } ?>
 </div>
