f607efd2ebf3898ae7269ed6dc611aec39ab65b5
Aless55 <aless1997@windowslive.com>
2021-05-05 11:24:33 +0200

    js changed

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,6 @@
 // JS Script to load in header
-$('button.menu-toggle').on('click', function(){
-    $('body').toggleClass('nav-is-toggled');
-});
+$(function(){
+	$('button.menu-toggle').on('click', function(){
+	    $('body').toggleClass('nav-is-toggled');
+	});
+})
