4d1e8536b60dca9b9b1be65399c90bea23f813fc
Samuel Schwegler <samuel.schwegler@gmx.ch>
2021-05-05 12:48:56 +0200

    content within title

--- blocks/block-card.php
+++ blocks/block-card.php
@@ -1,7 +1,7 @@
 <div class="card card-rows-<?php block_field('rows'); ?>">
     <img src="<?php block_field('bild'); ?>" alt="" class="card-hover-img">
-    <span class="card-title"><?php block_field('titel'); ?></span>
     <div class="card-content">
+        <span class="card-title"><?php block_field('titel'); ?></span>
         <?php if(block_value('text') !== "") { ?>
             <p><?php block_field('text'); ?></p>
         <?php } ?>
