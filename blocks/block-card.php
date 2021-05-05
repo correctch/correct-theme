ef6812a7cf13c53ace35e84ec687ba7a8f644b9d
Samuel Schwegler <samuel.schwegler@gmx.ch>
2021-05-05 12:42:40 +0200

    card content

--- blocks/block-card.php
+++ blocks/block-card.php
@@ -1,10 +1,12 @@
 <div class="card card-rows-<?php block_field('rows'); ?>">
     <img src="<?php block_field('bild'); ?>" alt="" class="card-hover-img">
     <span class="card-title"><?php block_field('titel'); ?></span>
-    <?php if(block_value('text') !== "") { ?>
-        <p><?php block_field('text'); ?></p>
-    <?php } ?>
-    <?php if(block_value('url') !== "") { ?>
-        <a href="<?php block_field('url'); ?>" class="card-link button">mehr erfahren</a>
-    <?php } ?>
+    <div class="card-content">
+        <?php if(block_value('text') !== "") { ?>
+            <p><?php block_field('text'); ?></p>
+        <?php } ?>
+        <?php if(block_value('url') !== "") { ?>
+            <a href="<?php block_field('url'); ?>" class="card-link button">mehr erfahren</a>
+        <?php } ?>
+    </div>
 </div>
