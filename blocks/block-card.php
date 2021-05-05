916e30441f24a6f20766c8796dcd538711e8c6de
Samuel Schwegler <samuel.schwegler@gmx.ch>
2021-05-05 15:41:19 +0200

    try to remove gap in card

--- blocks/block-card.php
+++ blocks/block-card.php
@@ -6,6 +6,7 @@
     <img src="<?php echo $url; ?>" alt="" class="card-hover-img">
     <div class="card-content">
         <span class="card-title"><?php block_field('titel'); ?></span>
+        <?php var_dump(block_value('text')); ?>
         <?php if(block_value('text') !== "") { ?><p><?php block_field('text'); ?></p><?php } ?>
         <?php if(block_value('url') !== "") { ?><a href="<?php block_field('url'); ?>" class="card-link button">mehr erfahren</a><?php } ?>
     </div>
