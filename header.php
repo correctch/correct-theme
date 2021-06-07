ca808d0f23f46451d0de101045967ba965f1eece
SamuelSchwegler <samuel.schwegler@gmx.ch>
2021-06-07 12:09:48 +0200

    added favicons

--- header.php
+++ header.php
@@ -1,48 +1,61 @@
 <!DOCTYPE HTML>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="de" itemscope itemtype="http://schema.org/WebPage">
 <head>
-	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
 
-	<!-- Icons -->
-    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <!-- Icons -->
+    <link rel="apple-touch-icon" sizes="57x57"
+          href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60"
+          href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72"
+          href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76"
+          href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114"
+          href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120"
+          href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144"
+          href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152"
+          href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180"
+          href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"
+          href="<?php echo get_template_directory_uri(); ?>/img/favicons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32"
+          href="<?php echo get_template_directory_uri(); ?>/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96"
+          href="<?php echo get_template_directory_uri(); ?>/img/favicons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16"
+          href="<?php echo get_template_directory_uri(); ?>/img/favicons/favicon-16x16.png">
     <link rel="manifest" href="/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
-	<link href="https://fonts.googleapis.com/css2?family=Assistant:wght@600;700&display=swap" rel="stylesheet">
-	<link href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" rel="stylesheet" type="text/css">
-	<?php wp_head(); ?>
+    <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@600;700&display=swap" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" rel="stylesheet" type="text/css">
+    <?php wp_head(); ?>
 
-	<script src="https://code.jquery.com/jquery-3.5.1.min.js"
-	integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-	crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+            crossorigin="anonymous"></script>
     <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/public/js/script.js"></script>
     <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/public/js/defer.js" defer></script>
 </head>
 <body>
-	<div id="header">
-		<div class="container">
-			<div class="logo">
-				<?php if ( function_exists( 'the_custom_logo' ) ) {
-					the_custom_logo();
-				} ?>
-			</div>
-				<?php require('includes/nav.php'); ?>
-			<button class="menu-toggle"></button>
-		</div>
-		<?php require('includes/mobile-nav.php'); ?>
-	</div>
+<div id="header">
+    <div class="container">
+        <div class="logo">
+            <?php if (function_exists('the_custom_logo')) {
+                the_custom_logo();
+            } ?>
+        </div>
+        <?php require('includes/nav.php'); ?>
+        <button class="menu-toggle"></button>
+    </div>
+    <?php require('includes/mobile-nav.php'); ?>
+</div>
