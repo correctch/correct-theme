b28fafc4c733479fe16c7c8d3286317decf653de
SamuelSchwegler <samuel.schwegler@gmx.ch>
2021-06-07 13:46:36 +0200

    kleinee korrektur schirftgrösse

---
+++ category.php
@@ -0,0 +1,35 @@
+<?php get_header(); ?>
+    <div class="container">
+        <h2 class="page-title"><?php single_cat_title(); ?></h2>
+        <div class="post-boxes row">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <div class="post-box col">
+                    <?php if (has_post_thumbnail()) { ?>
+                        <div class="post-box-image-container">
+                            <a class="link" href="<?php the_permalink(); ?>">
+                                <?php if (wp_is_mobile()) {
+                                    the_post_thumbnail('card-medium');
+                                } else {
+                                    the_post_thumbnail('card');
+                                } ?>
+                            </a>
+                        </div>
+                    <?php } ?>
+                    <p class="blog-info"><?php the_author(); ?> | <?php the_time('d.m.Y'); ?></p>
+                    <h4 class="post-box-title">
+                        <a class="link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </h4>
+                    <?php the_excerpt(); ?>
+                    <a class="post-box-action button" href="<?php the_permalink(); ?>">weiter lesen</a>
+                </div>
+            <?php endwhile; endif; ?>
+        </div>
+        <?php the_posts_pagination([
+            'screen_reader_text' => 'weitere Beiträge anzeigen',
+            'aria_label' => 'Beiträge',
+            'next_text' => '>',
+            'prev_text' => '<'
+        ]); ?>
+    </div>
+    </div><!-- main -->
+<?php get_footer(null, ['show_post' => false]); ?>
