293054c610955ab52f8aad0dbe5d722211afcac2
SamuelSchwegler <samuel.schwegler@gmx.ch>
2021-06-07 12:05:14 +0200

    minor fixes

--- blocks/block-card.php
+++ blocks/block-card.php
@@ -3,7 +3,7 @@
         $attachment_id = block_value( 'bild' );
         $url = wp_get_attachment_image_src( $attachment_id, $size = 'card')[0];
     ?>
-    <img src="<?php echo $url; ?>" alt="" class="card-hover-img">
+    <img src="<?php echo $url; ?>" alt="" class="card-img">
     <div class="card-content">
         <span class="card-title"><?php block_field('titel'); ?></span>
         <?php if(strlen(block_value('text')) > 2) { ?><p><?php block_field('text'); ?></p><?php } ?>
