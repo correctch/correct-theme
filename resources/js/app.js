c61e86f0ff0f9b3a2a8981d2ca242f534436541f
Aless55 <aless1997@windowslive.com>
2021-11-24 13:11:48 +0100

    changes js compiling

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,14 @@
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+import Vue from 'vue/dist/vue.common.js';
+import VueCookies from 'vue-cookies'
+
+window.Vue = Vue;
+
+const app = new Vue({}).$mount('#app');
+
 // JS Script to load as last
 
 /**
@@ -169,7 +180,7 @@ window.addEventListener('message', event => {
         setCookie('hubspotRegistration', event.data.id, 365);
         is_hubspot_user = true;
         $('.download-direct').each(function (index, elem){
-           $(this).css('display', 'inline-block');
+            $(this).css('display', 'inline-block');
         });
         $('.modal-button').each(function (index, elem){
             $(this).css('display', 'none');
@@ -177,3 +188,10 @@ window.addEventListener('message', event => {
     }
 });
 
+
+// JS Script to load in header
+$(function(){
+    $('button.menu-toggle').on('click', function(){
+        $('body').toggleClass('nav-is-toggled');
+    });
+})
